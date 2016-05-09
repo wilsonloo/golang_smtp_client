8ee122402ecb9a76105cc3fe10715a6ef91c9b4f
wilsonloo <wilsonloo21@163.com>
2016-05-09 12:08:41 +0800

    优化对象引用方式

--- src/main.go
+++ src/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 // smtp_msg通道
-var smtp_msg_chan chan bytes.Buffer
+var smtp_msg_read_chan chan bytes.Buffer
 var smtp_msg_write_chan chan bytes.Buffer
 
 func main() {
@@ -20,16 +20,15 @@ func main() {
 	util.CheckErrorOrExit(err)
 	defer conn.Close()
 
-	// 开启独立的读协程
-	smtp_msg_chan = make(chan bytes.Buffer, 1)
-	smtp_msg_write_chan = make(chan bytes.Buffer, 1)
+	// 定义读写channel
+	smtp_msg_read_chan = make(chan bytes.Buffer, 32)
+	smtp_msg_write_chan = make(chan bytes.Buffer, 32)
 
-	go nws.ReadRoutine(conn, &smtp_msg_chan)
+	// 启动读写协程
+	go nws.ReadRoutine(&conn, &smtp_msg_read_chan)
+	go nws.WriteToServerRoutine(&conn, &smtp_msg_write_chan)
 
-	// 独立的写协程
-	go nws.WriteToServerRoutine(conn, &smtp_msg_write_chan)
-
-	handle_smtp_msg_routine(&smtp_msg_chan)
+	handle_smtp_msg_routine(&smtp_msg_read_chan)
 }
 
 // 处理 smtp 消息
@@ -37,7 +36,7 @@ func handle_smtp_msg_routine(msg_chan *chan bytes.Buffer) {
 	// 主协程处理逻辑
 	for {
 		fmt.Println("waiting msg...")
-		msg_raw := <-smtp_msg_chan
+		msg_raw := <-smtp_msg_read_chan
 		msg := (*bytes.Buffer)(&msg_raw)
 		fmt.Println("got msg: ", msg)
 
--- src/network_service/nws.go
+++ src/network_service/nws.go
@@ -9,7 +9,7 @@ import (
 )
 
 // 读操作
-func ReadRoutine(conn net.Conn, reader *chan bytes.Buffer) {
+func ReadRoutine(conn *net.Conn, reader *chan bytes.Buffer) {
 	// todo
 
 	msg_feedback := bytes.NewBuffer(nil)
@@ -17,12 +17,12 @@ func ReadRoutine(conn net.Conn, reader *chan bytes.Buffer) {
 
 	for {
 		// 因为采用了独立的读协程，此处采用阻塞读
-		transferred, err := conn.Read(buf[0:])
+		transferred, err := (*conn).Read(buf[0:])
 		util.CheckErrorOrExit(err)
 
 		msg_feedback.Write(buf[0:transferred])
 
-		// 这里假设没有粘包或者包不足的情况
+		// todo 这里假设没有粘包或者包不足的情况
 		*reader <- *msg_feedback
 
 		fmt.Println("reseted")
@@ -31,12 +31,12 @@ func ReadRoutine(conn net.Conn, reader *chan bytes.Buffer) {
 	}
 }
 
-func WriteToServerRoutine(conn net.Conn, writer *chan bytes.Buffer) {
+func WriteToServerRoutine(conn *net.Conn, writer *chan bytes.Buffer) {
 	// todo
 
 	for {
 		msg := <-*writer
 		msg2 := (bytes.Buffer)(msg)
-		conn.Write(msg2.Bytes())
+		(*conn).Write(msg2.Bytes())
 	}
 }
